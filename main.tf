cdbf3d8de7e39ef68e227b6a279f3568b2090648
Antoine Deleporte <adeleporte@vmware.com>
2021-07-19 15:44:31 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
+  }
+}
+
+provider "random" {
+  # Configuration options
+}
+
+variable "id" {
+  description = "Change this ID will change the pet name"
+  type        = string
+}
+
+resource "random_pet" "server" {
+  keepers = {
+    # Generate a new pet name each time we switch to a new AMI id
+    pet_id = "${var.id}"
+  }
+}
+
+output "pet_output" {
+  value       = random_pet.server.id
+  description = "The Pet name"
+}
